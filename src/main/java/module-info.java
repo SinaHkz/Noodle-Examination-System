a90eea3e80e7122695e7da93721ec3fe85b5978c
SinaHkz <sina.hakimzadeh.1401@gmail.com>
2024-01-03 09:12:32 +0330

    add log in and sign up page
    add log in and sign up page wiht tis dependencies like fontawesome

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.noodleexaminationsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.noodleexaminationsystem to javafx.fxml;
