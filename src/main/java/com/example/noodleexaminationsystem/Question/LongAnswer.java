87ebe175f4f2b35334e9eac91cbd30170f25fb5d
setayesh saeidi <seta.saeidi@gmail.com>
2024-01-04 09:53:16 +0330

    added course and usage of question ,and their getter setters ,added abstract method delete and create question

--- src/main/java/com/example/noodleexaminationsystem/Question/LongAnswer.java
+++ src/main/java/com/example/noodleexaminationsystem/Question/LongAnswer.java
@@ -1,13 +1,26 @@
 package com.example.noodleexaminationsystem.Question;
 
+import com.example.noodleexaminationsystem.Course.Course;
+import com.example.noodleexaminationsystem.DataBase;
 import com.example.noodleexaminationsystem.User.User;
 
 public class LongAnswer extends Question {
     private String answer;
 
-    public LongAnswer(long id, String question, User questionDesigner, String answer) {
-        super( question, questionDesigner);
+    public LongAnswer(Course course, String question, User questionDesigner, String answer) {
+        super(course, question, questionDesigner);
         this.answer = answer;
+    }
+
+    @Override
+    public void createQuestion(String question, User questionDesigner, Course course) {
+        Question question1 = new LongAnswer(course , question , questionDesigner , answer);
+        DataBase.getQuestions().put(course , question1);
+    }
+
+    @Override
+    public void deleteQuestion() {
+        DataBase.getQuestions().remove(this.getCourse());
     }
 
     public String getAnswer() {
