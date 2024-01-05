b6837e7be8c67ac4601d8510f78b75765675d097
shivizr <zareshiva12@gmail.com>
2024-01-05 21:18:52 +0330

    add Create Course Plan contoller

---
+++ src/main/java/com/example/noodleexaminationsystem/CreateCoursePlanController.java
@@ -0,0 +1,40 @@
+package com.example.noodleexaminationsystem;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateCoursePlanController implements Initializable {
+   @FXML
+   TextField courseName;
+   @FXML
+   TextField coursePictureAddress;
+   @FXML
+    ComboBox comboBox;
+
+   public void setCreateCoursePlanButton(){
+       boolean flag=false;
+       comboBox.setStyle("border-colored-bottom:#096dde");
+        if(courseName.getText().isEmpty()){
+            courseName.setStyle("-fx-prompt-text-fill: red");
+            flag=true;
+        }
+        if(coursePictureAddress.getText().isEmpty()){
+            coursePictureAddress.setStyle("-fx-prompt-text-fill: red");
+            flag=true;
+        }
+        if(comboBox.getSelectionModel().getSelectedItem() == null){
+            comboBox.setStyle("-fx-border-color: red");
+            flag = true;
+        }
+        if(flag) return;
+   }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
